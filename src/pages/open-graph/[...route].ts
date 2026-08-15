11f37201751aae20df6675779c1d58f9ba3e187c
fr1en16 <pilipuyk.21@gmail.com>
2026-08-15 13:17:42 +0500

    T15: add sitemap metadata and generated OG images

---
+++ src/pages/open-graph/[...route].ts
@@ -0,0 +1,38 @@
+import { getCollection } from 'astro:content';
+import { OGImageRoute } from 'astro-og-canvas';
+
+const cases = await getCollection('cases');
+const pages = Object.fromEntries(cases.map(({ id, data }) => [id, data]));
+
+export const { getStaticPaths, GET } = await OGImageRoute({
+  pages,
+  getImageOptions: (_path, page) => ({
+    title: page.title,
+    description: `${page.year} · ${page.summary}`,
+    bgGradient: [
+      [29, 29, 29],
+      [163, 42, 23],
+    ],
+    border: {
+      color: [253, 75, 50],
+      width: 18,
+      side: 'inline-start',
+    },
+    fonts: [
+      './public/fonts/GolosText-Regular.woff2',
+      './public/fonts/GolosText-Bold.woff2',
+    ],
+    font: {
+      title: {
+        families: ['Golos Text'],
+        weight: 'Bold',
+        size: 76,
+      },
+      description: {
+        families: ['Golos Text'],
+        size: 34,
+        color: [247, 200, 191],
+      },
+    },
+  }),
+});
